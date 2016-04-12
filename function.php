a6e2bf71a3c1d84c5486e9d63454bf5f8e84ce3e
thruthesky <thruthesky@gmail.com>
2016-04-13 00:03:03 +0900

    work for app

--- function.php
+++ function.php
@@ -36,28 +36,31 @@ function test( $re, $message = null ) {
     }
 }
 
-/**
- * @param null $n
- * @return array|null
- */
-function segments($n = NULL) {
-    $u = strtolower(site_url());
-    $u = str_replace("http://", '', $u);
-    $u = str_replace("https://", '', $u);
-    $r = strtolower($_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']);
-    $uri = str_replace( "$u/", '', $r);
-    $arr = explode('?', $uri);
-	if ( $arr ) {
-	    $re = explode('/', $arr[0]);
-	}
-    if ( $n !== NULL ) {
-        if ( isset($re[$n]) ) return $re[$n];
-        else return NULL;
+if ( ! function_exists('segments') ) {
+    /**
+     * @param null $n
+     * @return array|null
+     */
+    function segments($n = NULL) {
+        $u = strtolower(site_url());
+        $u = str_replace("http://", '', $u);
+        $u = str_replace("https://", '', $u);
+        $r = strtolower($_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']);
+        $uri = str_replace( "$u/", '', $r);
+        $arr = explode('?', $uri);
+        $re = [];
+        if ( $arr ) {
+            $re = explode('/', $arr[0]);
+        }
+        if ( $n !== NULL ) {
+            if ( isset($re[$n]) ) return $re[$n];
+            else return NULL;
+        }
+        else return $re;
     }
-    else return $re;
-}
-function segment($n) {
-    return segments($n);
+    function segment($n) {
+        return segments($n);
+    }
 }
 
 
